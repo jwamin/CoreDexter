3240229b67306f8df67670bae05bed3f0889e864
Joss Manger <joss.manger@langlandusa.com>
2019-01-30 14:26:46 -0500

    Initial Commit

---
+++ CoreDexter/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  CoreDexter
+//
+//  Created by Joss Manger on 1/30/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
