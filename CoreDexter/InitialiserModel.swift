7d1b5f322ea55bc6a41e5fe743382b16d7c7dcbe
Joss Manger <joss.manger@langlandusa.com>
2019-01-30 17:08:56 -0500

    broken. fix it.

--- CoreDexter/InitialiserModel.swift
+++ CoreDexter/InitialiserModel.swift
@@ -9,12 +9,74 @@
 import UIKit
 import Foundation
 
+struct PokeData {
+    let name:String
+    let region:String
+    let generation:String
+    let index:String
+}
+
 class Initialiser{
     
-    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let max = 151
     
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let dispatchGroup = DispatchGroup.init()
+    var pokeArray:[PokeData] = []
     init(){
         print(delegate)
+        
+        
+        
+    }
+    
+    func returnTask(index:Int) -> URLSessionDataTask?{
+        
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokedex/2") else {
+            return nil
+        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                
+            if(error != nil){
+                fatalError()
+            }
+            
+            guard let data = data else {
+                fatalError("no data")
+            }
+            
+            let dexString = String(bytes: data, encoding: .utf8)!
+            let dexDict:[String:Any]
+            do{
+            dexDict = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:Any]
+            } catch {
+                fatalError()
+            }
+            
+            let pokeDict = dexDict["pokemon_entries"] as! [Any]
+            var pokies:[PokeData] = []
+            for poke in pokeDict{
+                let pokeAny = poke as! [String:Any]
+                let name = "whaaa"
+                let number = String(index)
+                let generation = "generation-i"
+                let region = dexDict["name"] as! String
+                let poke = PokeData(name: name, region: region, generation: generation, index: number)
+                pokies.append(poke)
+            }
+            
+            
+            
+            DispatchQueue.main.async {
+                self.pokeArray = pokies
+            }
+            
+            
+            self.dispatchGroup.leave()
+            
+            })
+        return task
     }
     
 }
--- CoreDexter/MasterViewController.swift
+++ CoreDexter/MasterViewController.swift
@@ -20,8 +20,8 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+//        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
