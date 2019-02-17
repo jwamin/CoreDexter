9f3b8857d3169b20ba40c7a2499988f8f6dd13e7
Joss Manger <Jwam3rd@gmail.com>
2019-02-17 18:31:06 -0500

    moves visualize into framework

---
+++ PokemonVisualiser-tvos/PokemonVisualiser_tvos.h
@@ -0,0 +1,19 @@
+//
+//  PokemonVisualiser_tvos.h
+//  PokemonVisualiser-tvos
+//
+//  Created by Joss Manger on 2/17/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokemonVisualiser_tvos.
+FOUNDATION_EXPORT double PokemonVisualiser_tvosVersionNumber;
+
+//! Project version string for PokemonVisualiser_tvos.
+FOUNDATION_EXPORT const unsigned char PokemonVisualiser_tvosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokemonVisualiser_tvos/PublicHeader.h>
+
+
